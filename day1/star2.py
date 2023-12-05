657265b82fca1f568e69e4212011ac6e91492509
Nicholas Rostant <rostant.nicholas@gmail.com>
2023-12-05 12:04:35 +0000

    Fix day 1 star 2

--- day1/star2.py
+++ day1/star2.py
@@ -1,3 +1,13 @@
+DIGITDICT = {"zero":"0",
+                "one":"1",
+                "two":"2",
+                "three":"3",
+                "four":"4",
+                "five":"5",
+                "six":"6",
+                "seven":"7",
+                "eight":"8",
+                "nine":"9"}
 
 with open("./day1/input.txt", 'r') as f:
     input = f.readlines()
@@ -5,29 +15,32 @@ with open("./day1/input.txt", 'r') as f:
 
 
 def processline(line:str)->int:
-    digits = [char for char in line if char.isnumeric()]
-    # print(f"{line} {(digits[0] + digits[-1])}")
-    return (digits[0] + digits[-1])
+    fline = texttodigit(line, False)
+    rline = texttodigit(line, True)
+    fdigits = [char for char in fline if char.isnumeric()]
+    rdigits = [char for char in rline if char.isnumeric()]
+    return (fdigits[0] + rdigits[-1])
 
-def texttodigit(line:str)->str:
-    digitdict = {"zero":"0",
-                 "one":"1",
-                 "two":"2",
-                 "three":"3",
-                 "four":"4",
-                 "five":"5",
-                 "six":"6",
-                 "seven":"7",
-                 "eight":"8",
-                 "nine":"9"}
-    for digit in digitdict:
-        line = str.translate(line,digitdict)
-        # line = line.replace(digit,str(digitdict[digit]))
-
-    return(line)
+def texttodigit(line:str, direction:bool)->str:
+    # Process sequentially
+    subline = ''
+    if direction == False:
+        for char in line:
+            subline += char
+            for digit in DIGITDICT:
+                if digit in subline:
+                    subline = subline.replace(digit, DIGITDICT[digit])
+    else:
+        for char in reversed(line):
+            subline = char + subline
+            for digit in DIGITDICT:
+                if digit in subline:
+                    subline = subline.replace(digit, DIGITDICT[digit])
+      
+    return subline
 
 for i in input:
-    print(f"{i} -> {texttodigit(i)} -> {processline(texttodigit(i))}")
-results = sum([int(processline(texttodigit(i))) for i in input])
+    print(f"{i} -> {texttodigit(i,False)} -> {texttodigit(i,True)} -> {processline(i)}")
+    results = sum([int(processline(i)) for i in input])
 
 print(results)
